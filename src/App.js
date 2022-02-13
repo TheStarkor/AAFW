83b0b8a675e2f031ba8543a3804ed605ad8ba831
thestarkor <sk_and_mc@kaist.ac.kr>
2022-02-13 00:28:56 -0800

    img 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./main";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      AAFW
+      <Main />
       <Footer />
     </div>
   );
