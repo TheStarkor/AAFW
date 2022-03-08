ae27714d15b00c5403f2cf6c6eef303829fca357
Eunhee-Jung <63434233+Eunhee-Jung@users.noreply.github.com>
2022-03-09 01:03:32 +0900

    domain

--- src/main/face.js
+++ src/main/face.js
@@ -27,7 +27,37 @@ function Face() {
       const resizedDetections = faceapi.resizeResults(detections, displaySize);
       canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight)
       console.log(detections)
-      faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
+      //faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
+
+
+      if (resizedDetections) {
+        resizedDetections.forEach((detection) => {
+          const text = [
+            '36.5',
+          ]
+          const anchor = { x: detection.box.x + detection.box.width / 2 - detection.box.width/5, y: detection.box.y - detection.box.width/3 }
+          // see DrawTextField below
+          const drawOptions = {
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            fontColor: '#22F529',
+            fontSize: detection.box.width/6,
+            padding: 10
+          }
+          const box = { x: detection.box.x, y: detection.box.y, width: detection.box.width, height: detection.box.height }
+          const boxOptions = {
+            boxColor: '#22F529',
+            lineWidth: 5
+          }
+          
+          const drawBox = new faceapi.draw.DrawBox(box, boxOptions)
+          drawBox.draw(document.getElementById('canvas'))
+
+          const drawTextBox = new faceapi.draw.DrawTextField(text, anchor, drawOptions)
+          drawTextBox.draw(document.getElementById('canvas'))
+        })
+      }
+
+
 
       const video = document.getElementById('video')
       const canvasSave = document.getElementById('2d');
@@ -44,7 +74,7 @@ function Face() {
           console.log(err)
         }
       })
-    }, 6000)
+    }, 150)
   }
   
   useEffect(() => {
